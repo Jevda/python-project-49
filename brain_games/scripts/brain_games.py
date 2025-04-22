ec0941314699b6bc243fa00c49f010cff64a4976
Jevda <alekzanya@gmail.com>
2025-04-22 15:03:27 +0300

    Шаг 1: Инициализация проекта, добавление структуры и точки входа

---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""Main script for brain-games."""
+
+
+def main():
+    """Run greeting."""
+    print("Welcome to the Brain Games!")
+
+
+if __name__ == '__main__':
+    main()
